ec924de9cedfbbec311cd33fbedcb7505840f379
Demetra Koras <demms@me.com>
2017-02-10 10:18:26 -0500

    Completes Adventure Mode

--- blackjack.rb
+++ blackjack.rb
@@ -16,17 +16,21 @@ class Game
       @player_one_hand << deck.draw_card
       @player_two_hand << deck.draw_card
     end
-    reveal_first_card
+    reveal_card
   end
 
 # Reveals first card
-  def reveal_first_card
-    puts "\nDealer reveals:"
-    dealer_card = player_two_hand[0]
-    puts "#{dealer_card.face} of #{dealer_card.suit}"
-    puts "\nYou have:"
-    show_player_hand
-    calculate_player_hand
+  def reveal_card
+    if total_hand(player_two_hand) == 21
+      puts "YOU LOST. Dealer has 21"
+      show_dealer_hand
+    elsif total_hand(player_two_hand) < 21
+      puts "\nDealer reveals:"
+      puts "#{player_two_hand.last.face} of #{player_two_hand.last.suit}"
+      puts "\nYou have:"
+      show_player_hand
+      calculate_player_hand
+    end
   end
 
 # Reveals player hand
@@ -41,22 +45,33 @@ class Game
     player_hand.inject(0) { |sum, card| sum += card.value }
   end
 
+# Reveals dealer hand
+  def show_dealer_hand
+    puts "\nDealer hand:"
+    player_two_hand.each do |card|
+      puts "#{card.face} of #{card.suit}"
+    end
+  end
+
 # Checks for initial win/loss
   def calculate_player_hand
     if total_hand(player_one_hand) == 21
       puts "YOU WON"
       play_again?
     elsif total_hand(player_one_hand) > 21
-      show_dealer_hand
+      # I don't know if show_dealer_hand is necessary here - confuses output
+      # show_dealer_hand
       puts "Bust! YOU LOST"
       play_again?
+    elsif player_one_hand.length > 5
+      puts "YOU WON"
     else
-      play_hand
+      play_player_hand
     end
   end
 
 # Play a hand
-  def play_hand
+  def play_player_hand
     response = prompt.select("Would you like to hit or stay?", %w(hit stay))
     if response == "hit"
       @player_one_hand << deck.draw_card
@@ -67,15 +82,6 @@ class Game
     end
   end
 
-# Reveals dealer hand
-  def show_dealer_hand
-    puts "\nDealer hand:"
-    player_two_hand.each do |card|
-      puts "#{card.face} of #{card.suit}"
-    end
-    # play_dealer_hand
-  end
-
 # Calculates total of dealer hand
   def play_dealer_hand
     show_dealer_hand
@@ -83,10 +89,12 @@ class Game
       puts "YOU WON"
       play_again?
     elsif total_hand(player_two_hand) == total_hand(player_one_hand)
-      puts "YOU WON"
-      play_again?
+      hands_tied
+      # puts "YOU WON"
+      # play_again?
     elsif total_hand(player_two_hand) > total_hand(player_one_hand)
       puts "YOU LOST"
+      play_again?
     elsif total_hand(player_two_hand) < 16
       @player_two_hand << deck.draw_card
       play_dealer_hand
@@ -96,6 +104,19 @@ class Game
     end
   end
 
+  def hands_tied
+    p1_length = player_one_hand.length
+    p2_length = player_two_hand.length
+    if p1_length < p2_length
+      puts "YOU LOST"
+      play_again?
+    else
+      puts "YOU WON"
+      play_again?
+    end
+  end
+
+# Asks to play again
   def play_again?
     response = prompt.select("Would you like to play again?", %w(Yes No))
     if response == "Yes"
@@ -104,8 +125,6 @@ class Game
       puts "Goodbye!"
     end
   end
-
-
 end
 
 Game.new
